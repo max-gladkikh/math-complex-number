3607f476a07e6da9e95918f117585ee4a4c22f5c
MaxGladkikh <max.gdkh@gmail.com>
2024-08-09 20:10:17 +0500

    changed syntax for php 5.6.0

--- tests/ComplexNumber_multiplication_Test.php
+++ tests/ComplexNumber_multiplication_Test.php
@@ -1,5 +1,4 @@
-<?php 
-declare(strict_types=1);
+<?php
 
 namespace ComplexMath\Tests;
 
@@ -48,7 +47,7 @@ final class ComplexNumber_multiplication_Test extends TestCase
         $this->assertEquals($resSecondMulFirst->getImaginary(), $resFirstMulSecond->getImaginary());
     }
 
-    public function getComplexNumbersParams(): array
+    public function getComplexNumbersParams()
     {
         return [
             [ // свойство нуля
